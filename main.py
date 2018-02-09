5d26a2606bdc642544e1d0b6b5deb1f64d12d8b4
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2018-02-08 23:54:21 -0700

    print out lecture names

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ def main():
     url = 'https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-034-artificial-intelligence-fall-2010/lecture-videos/'
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
-    print(soup.prettify())
-    for link in soup.find_all('a'):
-        print(link)
+
+    links = map(lambda x: x.string, soup.find_all('a'))
+
+    for s in links:
+        if str(s).startswith('Lecture'):
+            print(s)
 
 if __name__ == '__main__':
     main()
